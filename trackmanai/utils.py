40daf681a07b4acda6df31249185ed874ac3054e
QuentinBehar <behar.quentin@gmail.com>
2022-08-20 14:18:40 +0200

    Added MapLoader
    Added mapload function that loads a map.
    Will be part of the training mechanism to increase the difficulty of maps  when AI becomes better

--- trackmanai/utils.py
+++ trackmanai/utils.py
@@ -11,6 +11,7 @@ from tminterface.client import Client
 from tminterface.interface import TMInterface
 from tminterface.constants import DEFAULT_SERVER_SIZE
 import time
+import signal
 
 
 # Classes
@@ -264,3 +265,50 @@ def get_pix_lines(n_lines):
     for i in range(len(L_end_points)):
         L_pix_lines.append(getLine(c[0],c[1],L_end_points[i][0],L_end_points[i][1]))
     return L_pix_lines
+
+
+class MapLoader(Client):
+    def __init__(self, map):
+        super(MapLoader,self).__init__()
+        self.map=map
+        self.finished=False
+
+    def on_registered(self, iface: TMInterface):
+        """ A callback that the client has registered to a TMInterface instance.
+        """
+        print(f'Registered to {iface.server_name}')
+        #iface.log("Ready. Genome id: " + str(self.genome_id))
+        #set gamespeed
+        iface.set_timeout(5000)
+        iface.execute_command("map "+self.map)
+        iface.give_up()
+        iface.close()
+        self.finished=True
+    
+
+    def on_deregistered(self,iface):
+        """ A callback that the client has been deregistered from a TMInterface instance. 
+        This can be emitted when the game closes, the client does not respond in the timeout window, 
+        or the user manually deregisters the client with the deregister command.
+        """
+        print(f'deregistered to {iface.server_name}')
+
+def LoadMap(map):
+    client=MapLoader(map)
+    buffer_size=DEFAULT_SERVER_SIZE
+    server_name="TMInterface0"
+    iface = TMInterface(server_name, buffer_size)
+
+    def handler(signum, frame):
+        iface.close()
+
+    # Close connections
+    signal.signal(signal.SIGBREAK, handler)
+    signal.signal(signal.SIGINT, handler)
+
+    # Register a new client
+    iface.register(client)
+    while not client.finished:
+        time.sleep(0)
+    iface.close()
+    time.sleep(5)
